13680ec1d95ecdab5e61217695c367a6fc415dd7
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-09 19:24:34 -0800

    update favicon, readme, descriptions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+#To deploy
+npm run build
+firebase deploy
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/Interests/Descriptions.js
+++ src/components/Interests/Descriptions.js
@@ -5,24 +5,29 @@ export const Descriptions = {
     <div>
       When I was in middle school one of my good friends had introduced me to
       photography. I really enjoyed the idea of capturing important and unique
-      photos in creative ways. Every photo has a unique story and going back and
-      looking at photos and videos is really the only way you can relive those
-      moments. To this day I still really enjoy doing photography when I can. I
-      have posted a lot of my work to Flickr if you would like to check it out.{" "}
+      moments in creative ways. Every photo has a unique story and going back
+      and looking at photos and videos is really the only way you can relive
+      those moments. To this day I still really enjoy doing photography when I
+      can. I have posted a lot of my work to Flickr if you would like to check
+      it out.{" "}
       <a href="https://www.flickr.com/photos/nikonparty/">Flickr Link</a>
     </div>
   ),
   computers: (
     <div>
-      I have always been fascinated with computers for nearly as long as I can remember. They've gone from being extremely expensive and very limiting to be very cheap and extremely robust. I always love learning new things and that's why computers and programming are always fun. I can tinker with it all day every day and I will never know everything there is to know.
+      I have always been fascinated with computers for nearly as long as I can
+      remember. They've gone from being extremely expensive and very limiting to
+      be very cheap and extremely robust. I love learning new things and that's
+      why computers and programming are always fun. I can tinker with it all day
+      every day and I will never know everything there is to know.
     </div>
   ),
   energyVehicles: (
     <div>
       I really enjoy renewable energy, electric vehicles, as well as autonomous
       vehicles because of their ability to make society better, safer, and more
-      efficient. These are all things that given the opportunity I would love to
-      contribute to one day. Until then I love doing little side projects and
+      efficient. These are all things that, given the opportunity, I would love
+      to contribute to one day. Until then I love doing little side projects and
       keeping up with the advancements in these fields.
     </div>
   ),
@@ -35,8 +40,8 @@ export const Descriptions = {
       and I learned a lot. Following this, I joined the Air Force and went
       through electronics course and that laid the foundation to some of the
       hobby projects that I build now using Arduinos and Raspberry Pis. I am
-      also happy to report I have built a couple more electric bikes that have
-      not caught on fire.
+      also happy to report I have built more electric bikes that have not caught
+      on fire.
     </div>
   )
 }
