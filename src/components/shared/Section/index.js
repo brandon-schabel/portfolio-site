25a96076ae29447e891701100326913a45da62b2
brandon-schabel <brandonschabel1995@gmail.com>
2019-03-12 12:55:11 -0700

    remove console log

--- src/components/shared/Section/index.js
+++ src/components/shared/Section/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {StyledSection} from './styles'
 
 const Section = (props) => {
-  console.log(props)
   return (
     <StyledSection {...props}>
       {props.children}
